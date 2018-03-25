de515cf4564568e419057003d72c7e65bf4b24ca
Sebastian Madejski <madejski.s97@gmail.com>
2018-03-25 23:21:31 +0200

    update tests for ClientStorageEngine

--- src/test/java/po2/BankSystem/ClientStorageEngineTests.java
+++ src/test/java/po2/BankSystem/ClientStorageEngineTests.java
@@ -1,7 +1,9 @@
 package po2.BankSystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,8 +25,8 @@ public class ClientStorageEngineTests {
 	@Test
 	void addClientTest() {
 		sut.addClient(new Client(1337,"Jan","Kowalski",73010234567L,"ul. Szkolna 17, Białystok",6.66));
-		sut.addClient(new Client(1338,"Piotr","Jaworski",73010234567L,"ul. Młynarska 14/18 m. 16, 91-050 Łódź",6.66));
-		sut.addClient(new Client(1339,"Tomasz","Duch",73010234567L,"ul. Szkolna 17, Białystok",6.66));
+		sut.addClient(new Client(1338,"Piotr","Graczyk",73010234567L,"ul. Młynarska 14/18 m. 16, 91-050 Łódź",6.66));
+		sut.addClient(new Client(1339,"Tomasz","Lesiak",73010234567L,"ul. Szkolna 17, Białystok",6.66));
 		sut.addClient(new Client(1340,"gagad,gapdgk","fasfasf",73010234567L,"ul. Szkolna 17, Białystok",6.66));
 		sut.addClient(new Client(1341,"gadgdag","gadgki409i4903509w43tiopiopste[,yts0irjijrt0wopjntpoeropr",73010234567L,"ul. Szkolna 17, Białystok",6.66));
 		assertTrue(sut.commit());
@@ -32,7 +34,9 @@ public class ClientStorageEngineTests {
 	
 	@Test
 	void findClientTest() {
-		
+		assertNull(sut.findClient("invalid", Client.ClientCriteria.ADDRESS));
+		assertThrows(ClassCastException.class, () -> sut.findClient("invalid", Client.ClientCriteria.ID));
+		assertEquals(1339, sut.findClient("Lesiak", Client.ClientCriteria.SURNAME).getId());
 	}
 	
 	@AfterEach
