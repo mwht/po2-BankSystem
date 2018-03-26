aba1cda6c5527002ed6b9fe5981e4184284d1949
Sebastian Madejski <madejski.s97@gmail.com>
2018-03-26 17:35:39 +0200

    add actual money transfer

--- src/main/java/po2/BankSystem/TransferMoneyOperation.java
+++ src/main/java/po2/BankSystem/TransferMoneyOperation.java
@@ -4,14 +4,22 @@ public class TransferMoneyOperation extends Operation {
 	
 	private ClientStorageEngine cse;
 	private Client c;
+	private double balance;
 
 	public TransferMoneyOperation() {
 		cse = ClientStorageEngine.getInstance();
 		c = null;
+		setCommitMessage("Confirm money transfer? [y/n] ");
 	}
 	
 	@Override
 	public boolean commit() {
+		try {
+			c.addBalance(balance);
+		} catch(InvalidBalanceException ib) {
+			System.out.println("Error occured during money transfer: " + ib.getMessage());
+			return false;
+		}
 		return true;
 	}
 
@@ -25,13 +33,16 @@ public class TransferMoneyOperation extends Operation {
 		int id = getIntFromInput("Client ID: ");
 		try {
 			c = cse.findClient(id, Client.ClientCriteria.ID);
-			
+			System.out.print(c);
+			balance = getDoubleFromInput("Balance: ");
+		} catch(ClientNotFoundException csf) {
+			System.out.println("Client not found!");
 		}
 	}
 
 	@Override
 	public boolean isOperationPrivileged() {
-		return true;
+		return (c != null);
 	}
 
 }
