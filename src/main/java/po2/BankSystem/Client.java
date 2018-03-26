e6d668f132ff5c9207d7600d9c6821751f4fecd1
Sebastian Madejski <madejski.s97@gmail.com>
2018-03-26 13:13:54 +0200

    add Client stub documentation

--- src/main/java/po2/BankSystem/Client.java
+++ src/main/java/po2/BankSystem/Client.java
@@ -1,5 +1,11 @@
 package po2.BankSystem;
 
+/**
+ * Client is class which represents a bank client and contains methods
+ * for handling bank account balance.
+ * @author Sebastian Madejski
+ *
+ */
 public class Client {
 	private int id;
 	private String name;
@@ -7,15 +13,41 @@ public class Client {
 	private long pesel;
 	private String address;
 	private double balance;
+	/**
+	 * ClientCriteria is enumeration which specifies the criteria which client is
+	 * searched by for.
+	 * @author Sebastian Madejski
+	 *
+	 */
 	public enum ClientCriteria {
+		/**
+		 * Client ID
+		 */
 		ID,
+		/**
+		 * Client name
+		 */
 		NAME,
+		/**
+		 * Client surname
+		 */
 		SURNAME,
+		/**
+		 * Client PESEL
+		 */
 		PESEL,
+		/**
+		 * Client address
+		 */
 		ADDRESS,
+		/**
+		 * Client balance
+		 */
 		BALANCE
 	};
-	
+	/**
+	 * Client default constructor, initializes all client's values.
+	 */
 	public Client() {
 		id = 0;
 		name = "";
@@ -24,7 +56,15 @@ public class Client {
 		address = "";
 		balance = 0;
 	}
-	
+	/**
+	 * Client constructor which initializes all values by given parameters.
+	 * @param initialId client's initial ID
+	 * @param initialName client's initial name
+	 * @param initialSurname client's initial surname
+	 * @param initialPesel client's initial PESEL
+	 * @param initialAddress client's initial address
+	 * @param initialBalance client's initial balance
+	 */
 	public Client(int initialId, String initialName, String initialSurname, long initialPesel, String initialAddress, double initialBalance) {
 		id = initialId;
 		name = initialName;
@@ -34,26 +74,47 @@ public class Client {
 		balance = initialBalance;
 	}
 	
+	/**
+	 * Get client ID.
+	 * @return client's ID
+	 */
 	public int getId() {
 		return id;
 	}
 	 
+	/**
+	 * Set new client ID.
+	 * @param newId client's ID
+	 */
 	public void setId(int newId) {
 		id = newId;
 	}
 	
+	/**
+	 * Get client's name.
+	 * @return client's name
+	 */
 	public String getName() {
 		return name;
 	}
 	 
+	/**
+	 * Set new client's name.
+	 * @param newName new client's name
+	 */
 	public void setName(String newName) {
 		name = newName;
 	}
+	
 	
 	public String getSurname() {
 		return surname;
 	}
 	 
+	/**
+	 * Set new client's surname.
+	 * @param newSurname new client's surname
+	 */
 	public void setSurname(String newSurname) {
 		surname = newSurname;
 	}
@@ -62,6 +123,10 @@ public class Client {
 		return pesel;
 	}
 	 
+	/**
+	 * Set new client's PESEL.
+	 * @param newPesel new client's PESEL
+	 */
 	public void setPesel(long newPesel) {
 		pesel = newPesel;
 	}
@@ -69,7 +134,11 @@ public class Client {
 	public String getAddress() {
 		return address;
 	}
-	 
+	
+	/**
+	 * Set new client's address.
+	 * @param newAddress new client's address
+	 */ 
 	public void setAddress(String newAddress) {
 		address = newAddress;
 	}
@@ -78,6 +147,10 @@ public class Client {
 		return balance;
 	}
 	 
+	/**
+	 * Set new client's balance.
+	 * @param newBalance new client's balance
+	 */
 	public void setBalance(double newBalance) throws InvalidBalanceException {
 		if (newBalance < 0.0) {
 			throw new InvalidBalanceException("Negative balance given.");
