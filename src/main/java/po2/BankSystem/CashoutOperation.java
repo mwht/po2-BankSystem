cee984753c89ef0dc2a92d0f233d30b3957ba18a
Sebastian Madejski <madejski.s97@gmail.com>
2018-03-26 17:57:48 +0200

    add cashout

--- src/main/java/po2/BankSystem/CashoutOperation.java
+++ src/main/java/po2/BankSystem/CashoutOperation.java
@@ -1,25 +1,51 @@
 package po2.BankSystem;
 
 public class CashoutOperation extends Operation {
+	
+	private ClientStorageEngine cse;
+	private Client c;
+	private double balance;
 
+	public CashoutOperation() {
+		cse = ClientStorageEngine.getInstance();
+		c = null;
+		setCommitMessage("Confirm money cashout? [y/n] ");
+	}
+	
 	@Override
 	public boolean commit() {
-		return false;
+		try {
+			c.subBalance(balance);
+		} catch(InvalidBalanceException ib) {
+			System.out.println("Error occured during money transfer: " + ib.getMessage());
+			return false;
+		}
+		System.out.println("Money cashed out successfully.");
+		return true;
 	}
 
 	@Override
 	public boolean rollback() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public void perform() {
-
+		System.out.println("Cashout from account:");
+		System.out.println("---------------------");
+		int id = getIntFromInput("Client ID: ");
+		try {
+			c = cse.findClient(id, Client.ClientCriteria.ID);
+			System.out.print(c);
+			balance = getDoubleFromInput("Balance: ");
+		} catch(ClientNotFoundException csf) {
+			System.out.println("Client not found!");
+		}
 	}
 
 	@Override
 	public boolean isOperationPrivileged() {
-		return false;
+		return (c != null);
 	}
 
 }
